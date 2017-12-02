fcbfea0c0db6057dcbdaec2c9f1c278a9a133458
ianbrobin <ianbrobin@yahoo.com>
2017-12-02 10:53:28 -0700

    Added signUp.php and signUpForm.php

---
+++ signUp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Welcome</title>
+</head>
+<body>
+<?php
+	$email = $_REQUEST['email'];
+	$pword = $_REQUEST['password'];
+	$pword2 = $_REQUEST['password2'];
+	
+?>
+</body>
+</html>
---
+++ signUpForm.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Sign Up</title>
+	</head>
+	<body>
+		<form method = "post"  action="signUp.php">
+			Email: <input type="text" name="email"/></br>
+			Password: <input type="password" name="password"/></br>
+			Retype Password: <input type="password" name="password2"/></br>
+			<input type="submit" value="submit" />
+		</form>
+	</body>
+</html>
