22954bfe4b55a92be28b6676573ed6f9d1db74fd
KeiranPirie <kepi9444@Colorado.edu>
2017-12-02 16:43:28 -0700

    added types

--- sources.php
+++ sources.php
@@ -2,17 +2,59 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Submit</title>
+<title>Links</title>
 </head>
 <body>
 <?php
-	echo "Input Received as:<br>";
+	
 	$topic = $_REQUEST['topic'];
 	$subTopic = $_REQUEST['subTopic'];
 	$difficulty = $_REQUEST['difficulty'];
+	if(!isser($topic)){
+		die("No topic input, please try again");
+	}
+
+	if(!isset($subTopic)){
+		die("No SubTopic input, please try again");
+	}
+
+	if(!isset($difficulty)){
+		die("No difficulty input, please try again");
+	}
+	
+	
 	echo "Topic = $topic<br>";
 	echo "SubTopic = $subTopic<br>";
 	echo "difficulty = $difficulty<br>";
 ?>
+<h2> Add Link to Page </h2>
+<form action="addLink.php" method="post">
+	Type: 
+	<select name="type" >
+		<option value='article'>Article</option>
+		<option value='tutorial'>Tutorial</option>
+		<option value='video'>Video</option>
+		<option value='paper'>Paper</option>
+		<option value='course'>Course</option>
+		,
+	</select><br>
+	Difficulty:
+	<select name="difficulty" >
+		<option value='0'>Easy</option>
+		<option value='1'>Medium</option>
+		<option value='2'>Hard</option>
+		
+	</select><br>
+	
+	URL:
+	<input name="URL" type="text"><br>
+	<!-- Data that was already passed earlier -->
+	<?php
+		//echo "<input type='hidden' name='topic' value='$topic'>";
+		echo "<input type='hidden' name='subTopic' value='$subTopic'>";
+
+	?>
+	<input type="submit" value="Submit">
+</form>
 </body>
 </html>
