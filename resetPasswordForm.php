56f1d22bb5144098e24799be2a4905573530717d
ianbrobin <ianbrobin@yahoo.com>
2017-12-05 13:41:58 -0700

    Made reset Password Form

---
+++ resetPasswordForm.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+</head>
+<body>
+<?php
+	include "included.php";
+	$userId = $_REQUEST['userId'];
+	$key = $_REQUEST['key'];
+?>
+<h1>Reset Password</h1>
+<form method = "post" action="resetPassword.php">
+	<?php
+		echo "<input type='hidden' name='userId' value='$userId'/>";
+		echo "<input type='hidden' name='key' value='$key'/>";
+	?>
+	New Password:<input type = 'text' name = 'password'/></br>
+	Retype Password:<input type = 'text' name = 'password2'/></br>
+	<input type="submit" value="submit"/>
+</form>
+<?php	
+	mysqli_close($connection);
+?>
+</body>
+</html>
